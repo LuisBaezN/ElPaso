2c1e7d800b07018424b7c6d67151ab8f8357348b
LuisBaezN <baez.201041081@gmail.com>
2022-08-16 20:04:17 -0500

    add: Reestructuración de los productos

--- src/script.js
+++ src/script.js
@@ -1,24 +1,123 @@
 'use strict';
 
-// ///////////////////////////////// Descripción //////////////////////////////////////////////
+/**
+ * Tamaño máximo de caracteres en la línea de un producto en el menú.
+ */
+const TAM = 33;
 
-////////////////////////// Comida ///////////////////////////////////
-const com1 = '<p>Comida 1 ......................................... $12</p>';
-const com2 = '<p>Comida 2 ......................................... $121</p>';
+// ///////////////////////////////// Productos //////////////////////////////////////////////
 
-////////////////////////// Bebidas ///////////////////////////////////
+var alimentos = [
+    {
+        nombre: 'Comida 1',
+        precio: 112,
+        foto: 'beb3.jpg',
+        ingredientes: 'Aquí van los ingredientes...',
+        htmlEstruct: function () {
+            return htmlFormat(this.nombre, this.precio);
+        }
+    },
+    {
+        nombre: 'Comida 2',
+        precio: 7,
+        foto: 'beb3.jpg',
+        ingredientes: 'Aquí van los ingredientes...',
+        htmlEstruct: function () {
+            return htmlFormat(this.nombre, this.precio);
+        }
+    }
+];
 
-const beb1 = '<p>Bebida 1 ......................................... $22</p>';
-const beb2 = '<p>Bebida 2 ......................................... $2</p>';
-const beb3 = '<p>Bebida 3 ......................................... $71</p>';
+var bebidas = [
+    {
+        nombre: 'Bebida 1',
+        precio: 22.50,
+        foto: 'beb3.jpg',
+        ingredientes: 'Aquí van los ingredientes...',
+        htmlEstruct: function () {
+            return htmlFormat(this.nombre, this.precio);
+        }
+    },
+    {
+        nombre: 'Bebida 2',
+        precio: 56.50,
+        foto: 'beb3.jpg',
+        ingredientes: 'Aquí van los ingredientes...',
+        htmlEstruct: function () {
+            return htmlFormat(this.nombre, this.precio);
+        }
+    },
+    {
+        nombre: 'Bebida 3',
+        precio: 71,
+        foto: 'beb3.jpg',
+        ingredientes: 'Aquí van los ingredientes...',
+        htmlEstruct: function () {
+            return htmlFormat(this.nombre, this.precio);
+        }
+    }
+];
 
-////////////////////////// Botana ///////////////////////////////////
-const bot1 = '<p>Botana 1 ......................................... $22</p>';
-const bot2 = '<p>Botana 2 ......................................... $2</p>';
-const bot3 = '<p>Botana 3 ......................................... $71</p>';
+var botanas = [
+    {
+        nombre: 'Botana 1',
+        precio: 2.50,
+        foto: 'beb3.jpg',
+        ingredientes: 'Aquí van los ingredientes...',
+        htmlEstruct: function () {
+            return htmlFormat(this.nombre, this.precio);
+        }
+    },
+    {
+        nombre: 'Botana 2',
+        precio: 27,
+        foto: 'beb3.jpg',
+        ingredientes: 'Aquí van los ingredientes...',
+        htmlEstruct: function () {
+            return htmlFormat(this.nombre, this.precio);
+        }
+    },
+    {
+        nombre: 'Botana 32',
+        precio: 17,
+        foto: 'beb3.jpg',
+        ingredientes: 'Aquí van los ingredientes...',
+        htmlEstruct: function () {
+            return htmlFormat(this.nombre, this.precio);
+        }
+    }
+];
+
+const com1 = alimentos[0].htmlEstruct();
+const com2 = alimentos[1].htmlEstruct();
+
+const beb1 = bebidas[0].htmlEstruct();
+const beb2 = bebidas[1].htmlEstruct();
+const beb3 = bebidas[2].htmlEstruct();
+
+const bot1 = botanas[0].htmlEstruct();
+const bot2 = botanas[1].htmlEstruct();
+const bot3 = botanas[2].htmlEstruct();
 
 ////////////////////////////////////// Functions ////////////////////////////////////
 
+/**
+ * Crea el formato necesario para mostrar el producto en el menú.
+ * @param {string} nombre - Nombre del producto.
+ * @param {number} precio - Precio del producto.
+ * @returns 
+ */
+function htmlFormat(nombre, precio){
+    let dot = '.';
+    precio = '$' + precio.toFixed(2);
+    let dots = (TAM - (precio.length + nombre.length))*2;
+
+    return `<p>${nombre} ${dot.repeat(dots)} ${precio}</p>`
+}
+
+/**
+ * Desplega los productos y precios del menú en el DOM.
+ */
 function ini(){
     const comida1 = document.getElementById('com1');
     const comida2 = document.getElementById('com2');
@@ -44,53 +143,75 @@ function ini(){
     botana3.innerHTML = bot3;
 }
 
+/**
+ * Desplega o quita los detalles de algún producto seleccionado, como foto e ingredientes.
+ * @param {object} item - Etiqueta que enlaza al producto.
+ */
 function det(item){
 
     switch (item.innerHTML){
         case com1:
-            item.innerHTML += '<div class="dpc-desc"><div class="dpc-dbox"><img src="assets/beb3.jpg" alt="Bebida 3" style="border-radius: 100px; width: 130px;"></div><div class="dpc-dbox"><p class="desc">Estos son sus ingredientes</p></div></div><br>';
+            item.innerHTML += htmlDetails(alimentos[0].nombre, alimentos[0].foto, alimentos[0].ingredientes);
             break;
         case com2:
-            item.innerHTML += '<div class="dpc-desc"><div class="dpc-dbox"><img src="assets/beb3.jpg" alt="Bebida 3" style="border-radius: 100px; width: 130px;"></div><div class="dpc-dbox"><p class="desc">Estos son sus ingredientes</p></div></div><br>';
+            item.innerHTML += htmlDetails(alimentos[1].nombre, alimentos[1].foto, alimentos[1].ingredientes);
             break;
         case beb1:
-            item.innerHTML += '<div class="dpc-desc"><div class="dpc-dbox"><img src="assets/beb3.jpg" alt="Bebida 3" style="border-radius: 100px; width: 130px;"></div><div class="dpc-dbox"><p class="desc">Estos son sus ingredientes</p></div></div><br>';
+            item.innerHTML += htmlDetails(bebidas[0].nombre, bebidas[0].foto, bebidas[0].ingredientes);
             break;
         case beb2:
-            item.innerHTML += '<div class="dpc-desc"><div class="dpc-dbox"><img src="assets/beb3.jpg" alt="Bebida 3" style="border-radius: 100px; width: 130px;"></div><div class="dpc-dbox"><p class="desc">Estos son sus ingredientes</p></div></div><br>';
+            item.innerHTML += htmlDetails(bebidas[1].nombre, bebidas[1].foto, bebidas[1].ingredientes);
             break;
         case beb3:
-            item.innerHTML += '<div class="dpc-desc"><div class="dpc-dbox"><img src="assets/beb3.jpg" alt="Bebida 3" style="border-radius: 100px; width: 130px;"></div><div class="dpc-dbox"><p class="desc">Estos son sus ingredientes</p></div></div><br>';
+            item.innerHTML += htmlDetails(bebidas[2].nombre, bebidas[2].foto, bebidas[2].ingredientes);
             break;
         case bot1:
-            item.innerHTML += '<div class="dpc-desc"><div class="dpc-dbox"><img src="assets/beb3.jpg" alt="Bebida 3" style="border-radius: 100px; width: 130px;"></div><div class="dpc-dbox"><p class="desc">Estos son sus ingredientes</p></div></div><br>';
+            item.innerHTML += htmlDetails(botanas[0].nombre, botanas[0].foto, botanas[0].ingredientes);
             break;
         case bot2:
-            item.innerHTML += '<div class="dpc-desc"><div class="dpc-dbox"><img src="assets/beb3.jpg" alt="Bebida 3" style="border-radius: 100px; width: 130px;"></div><div class="dpc-dbox"><p class="desc">Estos son sus ingredientes</p></div></div><br>';
+            item.innerHTML += htmlDetails(botanas[1].nombre, botanas[1].foto, botanas[1].ingredientes);
             break;
         case bot3:
-            item.innerHTML += '<div class="dpc-desc"><div class="dpc-dbox"><img src="assets/beb3.jpg" alt="Bebida 3" style="border-radius: 100px; width: 130px;"></div><div class="dpc-dbox"><p class="desc">Estos son sus ingredientes</p></div></div><br>';
+            item.innerHTML += htmlDetails(botanas[1].nombre, botanas[1].foto, botanas[1].ingredientes);
             break;
         default:
             item.innerHTML = del(item);
     }
 }
 
+/**
+ * Da formato necesario para mostrar los detalles del producto en el DOM.
+ * @param {string} nom - Nombre del producto. 
+ * @param {string} img - Nombre de la imagen a mostrar.
+ * @param {string} desc - Ingredientes del producto.
+ * @returns 
+ */
+function htmlDetails(nom, img, desc) {
+    return `<div class="dpc-desc"><div class="dpc-dbox"><img src="assets/${img}" alt="${nom}" style="border-radius: 100px; width: 130px;"></div><div class="dpc-dbox"><p class="f-desc">${desc}</p></div></div><br>`;
+}
+
+/**
+ * Borra los detalles del producto en el DOM.
+ * @param {object} cont - Etiqueta que enlaza al producto.
+ * @returns 
+ */
 function del(cont){
     let limit = '>';
     let con = 0;
-    for (let i = 0; i <= 70; i++){
-        if (cont.innerHTML[i] === limit){
+    let inc = 0;
+
+    while (con != 2){
+        if (cont.innerHTML[inc] === limit){
             con++;
-            if (con === 2){
-                return cont.innerHTML.slice(0,i+1)
-            }
         }
+        inc++;
     }
+
+    return cont.innerHTML.slice(0,inc);
 }
 
 ////////////////////////////////////// Main ///////////////////////////////////////
 
-console.log('running...');
+console.log('> Running app...');
 
-ini();
+ini();  
